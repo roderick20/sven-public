3db648a9bd6a938adb65c8afddbd316e2fe8f805
roderick20 <roderick20@hotmail.com>
2025-02-25 20:30:04 -0500

    1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import { Container } from 'react-bootstrap';
 
 function Footer() {
   return (
-    <footer className="bg-dark text-light text-center py-4 mt-5">
+    <footer className="bg-dark text-light text-center py-4 mt-5" style={{
+      position: 'fixed',
+    bottom: '0',
+    left: '0',
+    padding: '1rem 0',
+    zIndex: '1000'
+    }}>
       <Container>
         <p>&copy; {new Date().getFullYear()} Sven. Todos los derechos reservados. info@sven.pe</p>
       </Container>
